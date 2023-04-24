2ccda5c8e11df47bcd5673944a83aad8e8ae6fcd
hizumi <nyatel@yandex.ru>
2023-04-24 13:21:37 +0400

    циклы

---
+++ example1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Циклы, break, continue
+
+// int x = 5;
+// while(x > 0){
+//     x--;
+//     if(x == 3) continue;
+    
+//     Console.WriteLine($"{x} 111");
+//  //break - прерывает выполнение цикла
+//  //continue - возвращяет в начало цикла
+// } 
+// Console.WriteLine("OK");
+
+for (int i = 0; i < 10; i++) //переменная i существует только внутри цикла
+{
+    Console.WriteLine($"{i} 111");
+}
+Console.WriteLine("OK");
+//Console.WriteLine($"{i} 111"); //тут она уже не работает. прога ее не видит
+
